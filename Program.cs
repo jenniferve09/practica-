96bdfad4ff42bbe1db3a5b2da09107fa1e8dbe07
jenniferve09 <105527570+jenniferve09@users.noreply.github.com>
2022-06-13 20:06:18 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,36 +1,61 @@
-﻿using System; 
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tienda.Class;
 
-namespace ConsoleApp1;
-class Program
+namespace Tienda
 {
-    static void Main(string[] args)
+    internal class Program
     {
-        cuadrado figura1 = new cuadrado(10, 10, 10, 10, "cuadrado", "rosa");
-        figura1.imprimir();
-        
-
-        rectangulo figura2 = new rectangulo(10, 20, "rectangulo", "azul");
-        figura2.imprimir();
-        
-        
-
-        circulo figura3 = new circulo(3.14, 4, "circulo", "very peri");
-        figura3.imprimir();
-        
-
-
-        List<Interface> listaInterface1 = new List<Interface>();
-        listaInterface1.Add(figura1);
-        listaInterface1.Add(figura2);
-        listaInterface1.Add(figura3);
-
-
-        foreach (Interface item in listaInterface1)
+        static void Main(string[] args)
         {
-            item.calculo();
+            Local local = new Local("1", "RBlack", "0982340054", "Santa Marta");
+            Producto producto1 = new Producto("1", "Mangas cortas", "Rojo", "M", 4.50, 3, "Disponible");
+            Producto producto2 = new Producto("1", "Mangas largas", "Amarillo", "M", 5.00, 2, "Disponible");
+            Cliente cliente1 = new Cliente("1", "Jose", "Chavez", "1728821082", "0986250734", "Jose@gmail.com", "Manta", "Leonidas Proaño");
+            Empleado empleado1 = new Empleado("1", "Evelyn", "Valdez", "187239231", "0982348011", "Eve@gmail.com", "Manta", "EveVaL", "29433958EV");
+
+            List<Producto> productos = new List<Producto>();
+            List<Cliente> clientes = new List<Cliente>();
+            List<Empleado> empleados = new List<Empleado>();
+            List<Factura> facturas = new List<Factura>();
+
+            clientes.Add(cliente1);
+            empleados.Add(empleado1);
+            productos.Add(producto1);
+            productos.Add(producto2);
+
+            for (int i = 1; i <= 3; i++)
+            {
+                foreach (Producto item in productos)
+                {
+                    if (item.Modelo.ToLower() == "mangas cortas" && item.Color.ToLower() == "rojo" && item.Stock > 0)
+                    {
+                        item.Stock = item.Stock - 1;
+                        Factura factura = new Factura("1", 0.12, "13/06/2022", item, i);
+                        facturas.Add(factura);
+                    }
+                }
+
+            }
+
+            Console.WriteLine("\t\t|------FACTURA------|\n" +
+                 "\nNombre: " + local.Nombre + "" +
+                 "\nTelefono: " + local.Telefono + "\n");
+
+            foreach (Factura item in facturas)
+            {
+                Console.WriteLine(item.ToString());
+
+            }
+            Console.WriteLine(facturas[1].valorPagar());
+
+
+            Console.ReadKey();
+
+
         }
     }
 }
-
-    
-    
